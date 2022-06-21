d6f4628345487be75837f46a26e075e0bba94257
Muhmmad Arif Rahman <m.arif.r123@gmail.com>
2022-06-21 20:12:09 +0700

    second commit

---
+++ InventoryService/Controllers/InventoryController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using InventoryService.Models;
+using InventoryService.Services;
+using Microsoft.AspNetCore.Http;
+
+namespace InventoryService.Controllers
+{
+    [Route("v1/")]
+    [ApiController]
+    public class InventoryController : ControllerBase
+    {
+        private readonly IInventoryServices _services;
+
+        [HttpPost]
+        [Route("AddInventoryItems")]
+        public ActionResult<InventoryItems> AddInventoryItems(InventoryItems items)
+        {
+            var inventoryItems = services.AddInventoryItems(items);
+
+            if (inventoryItems == null)
+            {
+                return NotFound();
+            }    
+            return Ok(inventoryItems);
+        }
+
+        [HttpGet]
+        [Route("GetInventoryItems")]
+        public ActionResult<Dictionary<string, InventoryItems>> GetInventoryItems()
+        {
+            var inventoryItems = _services.GetInventoryItems();
+
+            if (inventoryItems.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return inventoryItems;
+        }
+    }
+}
---
+++ InventoryService/Services/InventoryServices.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using InventoryService.Models;
+using InventoryService.Services;
+
+
+
+namespace InventoryService.Services
+{
+    public class InventoryServices : IInventoryServices
+    {
+        private readonly Dictionary<string, InventoryItems> _inventoryItems;
+
+        public InventoryServices()
+        {
+            _inventoryItems = new Dictionary<string, InventoryItems>();
+        }
+
+        public InventoryItems AddInventoryItem(InventoryItems items)
+        {
+            _inventoryItems.Add(items.ItemName, items);
+
+            return items;
+        }
+
+        public object AddInventoryItems(InventoryItems items)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
